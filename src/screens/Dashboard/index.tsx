5551f60b82abcff8581e5446984e2b61c043807b
Igor da Costa Quiterio <idcq101@gmail.com>
2021-06-16 19:56:08 -0300

    Cartões de Transações

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -10,9 +10,13 @@ import {
   UserName,
   UserWrapper,
   Icon,
-  HighlightCards
+  HighlightCards,
+  Transactions,
+  Title
 } from './styles';
+
 import { HighlightCard } from '../../components/HighlightCArd';
+import { TransactionCard } from '../../components/TransactionCard';
 
 
 export function Dashboard(){
@@ -36,10 +40,35 @@ export function Dashboard(){
       </Header>
 
       <HighlightCards>
-        <HighlightCard />
-        <HighlightCard />
-        <HighlightCard />
+        <HighlightCard 
+          type='up'
+          title="Entradas"
+          amount="R$ 17.400,00" 
+          lastTransaction="Última entrada dia 13 de abril"
+        />
+        <HighlightCard 
+          type='down'
+          title="Saídas" 
+          amount="R$ 1.259,00" 
+          lastTransaction="Última saída dia 03 de abril"
+        />
+        <HighlightCard 
+          type='total'
+          title="Total" 
+          amount="R$ 16.141,00" 
+          lastTransaction="01 à 16 de abril"
+        />
       </HighlightCards>
+    
+      <Transactions>
+        <Title>
+          Listagem
+        </Title>
+     
+       <TransactionCard/>
+        
+      </Transactions>
+   
     </Container>
   )
 }
